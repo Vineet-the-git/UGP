beed2ebe8c51f3057aa0ea9e974d865dfc7d5870
Vineet Kumar <vineet.kmr1708@gmail.com>
2024-04-02 11:05:37 +0530

    cross network added but not being trained, work on ipynb

--- scvi/models/__init__.py
+++ scvi/models/__init__.py
@@ -5,8 +5,12 @@ from .autozivae import AutoZIVAE
 from .vaec import VAEC
 from .jvae import JVAE
 from .totalvi import TOTALVI
+from .rna_unshared import RNA_UNSHARED
 from .proteinvi_unshared import PROTENVI_UNSHARED
 from .totalvi_shared import TOTALVI_SHARED
+from .totalvi_shared_cross import TOTALVI_SHARED_CROSS
+from .proteinvi_unshared_cross import PROTENVI_UNSHARED_CROSS
+from .rna_unshared_cross import RNA_UNSHARED_CROSS
 
 __all__ = [
     "SCANVI",
@@ -17,6 +21,10 @@ __all__ = [
     "Classifier",
     "AutoZIVAE",
     "TOTALVI",
+    "RNA_UNSHARED",
     "PROTENVI_UNSHARED",
     "TOTALVI_SHARED",
+    "TOTALVI_SHARED_CROSS",
+    "PROTENVI_UNSHARED_CROSS",
+    "RNA_UNSHARED_CROSS",
 ]
