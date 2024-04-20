9e1eca9f5f9ca22d4e23363c2161d21d0bf30528
Vineet Kumar <vineet.kmr1708@gmail.com>
2024-04-20 16:24:10 +0530

    all the ugp work

--- testing.py
+++ testing.py
@@ -1,12 +1,29 @@
-import torch
-from scvi.models.proteinvi_unshared import PROTENVI_UNSHARED
+import torch as th
+from scvi.models.midas import Net
 
+rna_encoder_dims = [1191, 1024]
+adt_encoder_dims = [54, 1024]
+shared_encoder_dims = [180, 1024]
+common_encoder_dims = [1024, 128, 68]
+common_decoder_dims = [34, 128, 1024]
+s_encoder_dims = [2, 16, 16, 68]
+s_decoder_dims = [2, 16, 16, 2]
+norm = "ln"
+drop = 0.2
+dim_z = 34
+
+#write testing code
 if __name__ == "__main__":
-    print("Hello from testing.py")
-    #create a random tensor of whole numbers of shape (4, 224)
-    x = torch.randint(0, 10, (4, 224))
-    #create a model
-    model = PROTENVI_UNSHARED(x.shape[1], latent_distribution="normal")
-    #forward pass
-    recon_loss, kl_div_z, kl_div_back_pro, z = model(x)
-    print(recon_loss.shape, kl_div_z.shape, kl_div_back_pro.shape, z.shape)
+    #initialize model
+    model = Net(rna_encoder_dims, adt_encoder_dims, shared_encoder_dims, common_encoder_dims, common_decoder_dims, s_encoder_dims, s_decoder_dims, norm, drop, dim_z)
+    #initialize input
+    inputs = {}
+    inputs["x_unshared"] = th.randn(256, 1191)
+    inputs["x_shared"] = th.randn(256, 180)
+    inputs["y_unshared"] = th.randn(256, 54)
+    inputs["y_shared"] = th.randn(256, 180)
+    print(inputs["x_unshared"].dtype)
+    #run model
+    loss, z = model(inputs)
+    print(loss)
+    print(z.shape)
